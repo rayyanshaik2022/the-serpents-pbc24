4d07fb21a870cf7bd01dd4b16d26e701a3fdd8be
Rahul Mohan <143127250+rmohan-ucf@users.noreply.github.com>
2024-02-23 20:17:09 -0500

    Restructured integration code

---
+++ server/api.js
@@ -0,0 +1,22 @@
+
+
+
+export async function loginEndpoint(req, res) {
+    
+}
+
+export async function getEntryCostEndpoint(req, res) {
+
+}
+
+export async function payEntryEndpoint(req, res) {
+
+}
+
+export async function getWithdrawalTotalEndpoint(req, res) {
+
+}
+
+export async function executeWithdrawalEndpoint(req, res) {
+
+}

f03a255a677ef399de7efaea1af981c01baa659c
Rayyan Shaik <158090281+rashaikupenn@users.noreply.github.com>
2024-02-23 19:45:00 -0500

    Added frontend ready for graphics

--- server/index.js
+++ server/index.js
@@ -20,21 +20,71 @@ app.get("/", (req, res) => {
     res.send("Hello world!");
 });
 
+// Game constants
+MAP_SIZE = 500
+
+class Ship {
+    constructor () {
+        this.position = [0, 0];
+        this.velocity = [0, 0];
+        
+        this.tokens = []
+        this.cannon_balls = []
+    }
+
+    update () {
+        const velocity = () => {
+            if (this.position[0] + this.velocity[1] > MAP_SIZE) { 
+                this.position[0] = MAP_SIZE;
+            } else if (this.position[0] + this.velocity[0] < MAP_SIZE) {
+                this.position[0] = 0;
+            }
+
+            if (this.position[1] + this.velocity[1] > MAP_SIZE) {
+                this.position[1] = MAP_SIZE;
+            } else if (this.position[1] + this.velocity[1] < MAP_SIZE) {
+                this.position[1] = MAP_SIZE;
+            }
+        }
+
+        velocity();
+    }
+}
+
+class Ball {
+    constructor (position, direction, speed) {
+        this.position = position
+        this.velocity = ""// calc velocity
+    }
+}
+
+let SERVER_DATA;
+const gameInit = () => {
+    SERVER_DATA = {
+        players : {},
+        tokens : []
+    }
+}
+
+
+gameInit()
+
 // Game loop interval (update every second)
 const gameLoopInterval = setInterval(() => {
     // Update the game state
 
     // Broadcast the updated game state to all connected clients
-    io.emit("game_state", { counter: gameCounter });
+    io.emit("game_state", { counter: 1 });
 }, 1000 / 60);
 
 io.on("connection", (socket) => {
     console.log("A user connected");
+    SERVER_DATA.players[socket.id] = new Ship();
+    console.log(SERVER_DATA)
 
     // Example: Listen for 'message' event from the client
     socket.on("message", (data) => {
         console.log("Message from client:", data);
-
         // Example: Broadcast the received message to all connected clients
         io.emit("message", `Server says: ${data}`);
     });
@@ -43,6 +93,7 @@ io.on("connection", (socket) => {
 
     socket.on("disconnect", () => {
         console.log("User disconnected");
+        delete SERVER_DATA.players[socket.id]
     });
 });
 
